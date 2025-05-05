f301fd31ee8819c3344e9595559ff0d6335b5338
Alk096 <mahamsdoualker@gmail.com>
2025-05-06 00:43:35 +0100

    correction

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,1 +1,13 @@
 # Script pour vérifier l'existence d'un fichier donné
+
+echo -n 'Entrez le nom du fichier : '
+read nomFichier
+
+# Vérification de l'existence du fichier
+if [ -z "$nomFichier" ]; then
+    echo "Vous n'avez pas entré de nom de fichier."
+elif [ -f "$nomFichier" ]; then
+    echo "Le fichier '$nomFichier' existe."
+else
+    echo "Le fichier '$nomFichier' n'existe pas."
+fi
--- create_directory.sh
+++ create_directory.sh
@@ -1,1 +1,8 @@
 # Script pour créer un répertoire "test_directory"
+
+if [ ! -d "test_directory" ]; then
+    mkdir test_directory
+    echo "Répertoire 'test_directory' créé."
+else
+    echo "Le répertoire 'test_directory' existe déjà."
+fi
