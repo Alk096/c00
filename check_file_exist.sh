6f4bc506f49566ec9429b61c8964c0cbada3d843
Alk096 <mahamsdoualker@gmail.com>
2025-05-06 22:32:48 +0100

    correction

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,9 +1,17 @@
 # Script pour vérifier l'existence d'un fichier donné
 
 echo -n 'Entrez le nom du fichier : '
-read nomFichier
+read filename
+
+# Vérification de l'entrée utilisateur
+if [ -z "$filename" ]; then
+    echo "Erreur : Vous n'avez pas entré de nom de fichier."
+    exit 1
+fi
 
 # Vérification de l'existence du fichier
-if [ -f "$nomFichier" ]; then
-    echo "Le fichier '$nomFichier' existe."
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
+else
+    echo "Le fichier '$filename' n'existe pas."
 fi
