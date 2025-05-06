fd3d56e83da0c0f7fb12df0ece744b9d3b0115c1
Alk096 <mahamsdoualker@gmail.com>
2025-05-06 20:42:52 +0100

    correction

---
+++ create_directory.sh
@@ -0,0 +1,8 @@
+# Script pour créer un répertoire "test_directory"
+
+if [ ! -d "test_directory" ]; then #Cette condiition verifie si le fichier existe deja
+    mkdir test_directory
+    echo "Répertoire 'test_directory' créé."
+else
+    echo "Le répertoire 'test_directory' existe déjà."
+fi
